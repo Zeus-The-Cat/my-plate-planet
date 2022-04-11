8c9b1318d94c3e8186e180d44596114782cb2046
Zeus-The-Cat <d.s.pettry@gmail.com>
2022-04-10 22:49:01 -0400

    debugging

--- pages/api/handler.js
+++ pages/api/handler.js
@@ -1,6 +1,6 @@
-import db from '../../utils/db';
+import db from './db';
 
-export default async function handler(req, res){
+export default async function handler(_, res){
   try {
     const entries = await db.collection('dataset').get();
     res.status(200).json({
